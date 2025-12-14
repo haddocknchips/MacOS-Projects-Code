fdf93ae679e1792010c230ae0eea5346faf0f802
haddocknchips <6503162+haddocknchips@users.noreply.github.com>
2025-12-14 01:03:04 -0500

    Updated Python Ch5

--- Python/Ch5.py
+++ Python/Ch5.py
@@ -218,3 +218,158 @@ elif alien_color == 'yellow':
     print("You just earned 10 points!")
 else:
     print("You just earned 15 points.")
+
+# 5-6 Stages of life
+
+print()
+age = 57
+if age < 2:
+    print("The person is a baby.")
+if age >= 2 and age < 4:
+    print("The person is a toddler.")
+if age >= 4 and age < 13:
+    print("The person is a kid.")
+if age >= 13 and age < 20:
+    print("The person is a teenager.")
+if age >= 20 and age < 65:
+    print("The person is an adult.")
+if age >= 65:
+    print("The person is an elder.")
+
+# 5-7 Favorite fruit
+
+print()
+favorite_fruits = ['watermelon', 'durian', 'guyabano', 'apple', 'orange']
+if 'apple' in favorite_fruits:
+    print("That's a favorite!")
+else:
+    print("That's not a favorite.")
+if 'mango' in favorite_fruits:
+    print("That's a favorite!")
+else:
+    print("That's not a favorite.")
+if 'strawberry' in favorite_fruits:
+    print("That's a favorite!")
+else:
+    print("That's not a favorite.")
+if 'orange' in favorite_fruits:
+    print("That's a favorite!")
+else:
+    print("That's not a favorite.")
+
+print()
+favorite_fruits = ['macopa', 'avocado', 'lychee']
+fruit = 'orange'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}s!")
+fruit = 'macopa'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}s!")
+fruit = 'rambutan'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}s!")
+fruit = 'watermelon'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}s!")
+fruit = 'avocado'
+if fruit in favorite_fruits:
+    print(f"You really like {fruit}s!")
+
+# pizza toppings example
+
+print()
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
+
+# Checking that a list is not empty.
+
+print()
+requested_toppings = ['olives']
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+# Using multiple lists
+
+print()
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!")
+
+# 5-8 Hello admin
+
+print()
+usernames = ['admin', 'paolo', 'pauline', 'giulio', 'emilio', 'cj', 'ogi']
+for user in usernames:
+    if user == 'admin':
+        print(f"Hello, {user.title()}. Would you like to see a status report?")
+    elif user == 'cj':
+        print(f"Hello, {user.upper()}. Thank you for logging in again.")
+    else:
+        print(f"Hello, {user.title()}. Thank you for logging in again.")
+
+# 5-9 No users
+
+print()
+#usernames = ['admin', 'paolo', 'pauline', 'giulio', 'emilio', 'cj', 'ogi']
+usernames = []
+
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print(f"Hello, {user.title()}. Would you like to see a status report?")
+        elif user == 'cj':
+            print(f"Hello, {user.upper()}. Thank you for logging in again.")
+        else:
+            print(f"Hello, {user.title()}. Thank you for logging in again.")
+else: 
+    print("We need to find some users!")
+
+# 5-10 Checking usernames
+
+print()
+current_users = ['Paolo', 'Pauline', 'Giulio', 'Emilio', 'CJ', 'Ogi']
+current_users_copy = [current_user.lower() for current_user in current_users]
+
+new_users = ['Emilio', 'Ogi', 'Rudolph', 'Jim', 'Gregga']
+new_users_copy = [new_user.lower() for new_user in new_users]
+
+for new_user_copy in new_users_copy:
+    if new_user_copy in current_users_copy:
+        print("That username is taken. You will need to choose a new username.")
+    else:
+        print("That username is available.")
+
+# 5-11 Ordinal numbers
+
+print()
+ordinals = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for ordinal in ordinals:
+    if ordinal == 1:
+        print("1st")
+    elif ordinal == 2:
+        print("2nd")
+    elif ordinal == 3:
+        print("3rd")
+    else:
+        print(f"{ordinal}th")
+
+# 5-12 Styling statements => checked
+
+# Your ideas => transpose to separate notes
+# - Horner's
+# - Finance and investing tool
--- Python/ideas.md
+++ Python/ideas.md
@@ -4,3 +4,4 @@
 ### Ideas and notes
 
 - Horner's system for desktop and iPhone
+- Tool to analyse data for investing, including using AI tools; quite vague, don't know yet what this is in detail
