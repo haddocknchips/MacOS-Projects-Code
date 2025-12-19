c49e206479076db02ffb47aeca98c72013723634
haddocknchips <6503162+haddocknchips@users.noreply.github.com>
2025-12-18 20:36:13 -0500

    Update Python Ch7

--- Python/ch7.py
+++ Python/ch7.py
@@ -67,4 +67,16 @@ while message != 'quit':
     if message != 'quit':
         print(message)
 
+# Using a flag
 
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to the the program. "
+
+active = True
+
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
